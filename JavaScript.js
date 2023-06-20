63207c00c2a66cae5544561688290ca893f3c785
JoseMera10 <96705068+JoseMera10@users.noreply.github.com>
2023-06-20 11:08:22 -0500

    Pagina_web

---
+++ JavaScript.js
@@ -0,0 +1,26 @@
+function operacion1()
+{
+    document.getElementById("res").innerHTML = "La Raiz es: "+ boton1();
+}
+function boton1()
+{
+    //Esta es una operacion
+    a=2
+    b=3
+    r=a+b
+    alert ("Hola, presionastes el boton 2"+ ", y la resta es: " + r)
+}
+
+//Deben crear la siguiente estructura segun su operacion
+function operacion2()
+{
+    document.getElementById("res").innerHTML = "La raiz es: " + boton2();
+}
+function boton2()
+{
+    //Esta es una operacion
+    a=9
+    b=4
+    r=a-b
+    alert ("Hola, la resta es: " + r)
+}
